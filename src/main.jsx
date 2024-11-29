999a1adb01c8069996f0ea81bc7d093191b4c4ed
Rohit Deshmukh <deshmukhrohit373@gmail.com>
2024-11-29 15:41:15 +0530

    retouch homepage

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
+// Don't Touch this
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
+// Import Components here if necessaary if not work only in App.jsx
 import App from './App.jsx'
 
+// Linking the Web Frame with Root
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
