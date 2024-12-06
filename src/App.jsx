14e9dfeb4ae310430e35b78bcad916c00ad66ef7
Rohit Deshmukh <deshmukhrohit373@gmail.com>
2024-12-06 05:56:22 +0530

    deleted unnecssary stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
 import LearnifyLogo from '/logo.png'
 import './App.css'
 
 function App() {
-
+ 
   return (
     <>
       <div>
-        <a href="https://learnify-coral.vercel.div" target="_blank">
+        <a href="https://vite.dev">
           <img src={LearnifyLogo} className="logo" alt="Learnify logo" />
         </a>
       </div>
       <h1>Learnify</h1>
-      <p className="read-the-docs">
-        Lets Begin another Project
-      </p>
+      <p>Using Vite + React</p>  
     </>
   )
 }
